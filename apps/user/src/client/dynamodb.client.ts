b25016c2c5858f228cca2ef37631c88693d4e642
Possanii <gpossanii@outlook.com>
2025-06-19 23:30:39 -0300

    feat: create dynamodb client and env config

---
+++ apps/user/src/client/dynamodb.client.ts
@@ -0,0 +1,83 @@
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import {
+  DeleteCommand,
+  DynamoDBDocumentClient,
+  GetCommand,
+  GetCommandOutput,
+  PutCommand,
+  UpdateCommand,
+} from '@aws-sdk/lib-dynamodb';
+import {
+  Inject,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { ILoggerClient, LOGGER_CLIENT_TOKEN } from './logger.client';
+
+export interface IDatabaseClient {
+  getItem(command: GetCommand): Promise<GetCommandOutput>;
+  putItem(command: PutCommand): Promise<void>;
+  updateItem(command: UpdateCommand): Promise<void>;
+  deleteItem(command: DeleteCommand): Promise<void>;
+}
+
+export const DYNAMODB_CLIENT_TOKEN = Symbol('DYNAMODB_CLIENT');
+
+@Injectable()
+export class DynamoClient implements IDatabaseClient {
+  private readonly instance: DynamoDBDocumentClient;
+
+  constructor(
+    @Inject(LOGGER_CLIENT_TOKEN) private readonly logger: ILoggerClient,
+  ) {
+    if (!this.instance) {
+      const client = new DynamoDBClient({
+        endpoint: process.env.DB_ENDPOINT,
+      });
+
+      this.instance = DynamoDBDocumentClient.from(client);
+    }
+
+    this.logger.log('DynamoDB client initialized');
+  }
+
+  public async getItem(command: GetCommand): Promise<GetCommandOutput> {
+    try {
+      this.logger.info('Getting item from DynamoDB');
+      return await this.instance.send(command);
+    } catch (error) {
+      this.logger.error('Error getting item from DynamoDB', error);
+      throw new InternalServerErrorException(error);
+    }
+  }
+
+  public async putItem(command: PutCommand): Promise<void> {
+    try {
+      this.logger.info('Putting item into DynamoDB');
+      await this.instance.send(command);
+    } catch (error) {
+      this.logger.error('Error putting item into DynamoDB', error);
+      throw new InternalServerErrorException(error);
+    }
+  }
+
+  async updateItem(command: UpdateCommand): Promise<void> {
+    try {
+      this.logger.info('Updating item into DynamoDB');
+      await this.instance.send(command);
+    } catch (error) {
+      this.logger.error('Error Updating item from DynamoDB', error);
+      throw new InternalServerErrorException(error);
+    }
+  }
+
+  async deleteItem(command: DeleteCommand): Promise<void> {
+    try {
+      this.logger.info('Deleting item from DynamoDB');
+      await this.instance.send(command);
+    } catch (error) {
+      this.logger.error('Error deleting item from DynamoDB', error);
+      throw new InternalServerErrorException(error);
+    }
+  }
+}
--- apps/user/src/user.module.ts
+++ apps/user/src/user.module.ts
@@ -1,10 +1,33 @@
-import {Module} from '@nestjs/common';
-import {UserController} from './controller/user.controller';
-import {UserUseCase} from "./usecase/user.usecase";
+import { Module } from '@nestjs/common';
+import { UserController } from './controller/user.controller';
+import {
+  CREATE_CUSTOMER_USE_CASE_TOKEN,
+  CreateCustomerUseCase,
+} from './usecase/create-customer.usecase';
+import { LOGGER_CLIENT_TOKEN, LoggerClient } from './client/logger.client';
+import { DynamoClient, DYNAMODB_CLIENT_TOKEN } from './client/dynamodb.client';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [UserController],
-  providers: [UserUseCase],
+  providers: [
+    {
+      provide: LOGGER_CLIENT_TOKEN,
+      useClass: LoggerClient,
+    },
+    {
+      provide: DYNAMODB_CLIENT_TOKEN,
+      useClass: DynamoClient,
+    },
+    {
+      provide: CREATE_CUSTOMER_USE_CASE_TOKEN,
+      useClass: CreateCustomerUseCase,
+    },
+  ],
 })
 export class UserModule {}
