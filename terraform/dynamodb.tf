a8e1e68a82b2e47d998373276bed095fd4823300
Possanii <gpossanii@outlook.com>
2025-06-29 13:02:33 -0300

    chore: create dynamo tables (products and purchases)

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,3 +1,25 @@
+resource "aws_dynamodb_table" "products" {
+    name           = "products"
+    billing_mode   = "PAY_PER_REQUEST"
+    hash_key       = "id"
+
+    attribute {
+        name = "id"
+        type = "S"
+    }
+}
+
+resource "aws_dynamodb_table" "purchases" {
+    name           = "purchases"
+    billing_mode   = "PAY_PER_REQUEST"
+    hash_key       = "id"
+
+    attribute {
+        name = "id"
+        type = "S"
+    }
+}
+
 resource "aws_dynamodb_table" "users" {
     name           = "users"
     billing_mode   = "PAY_PER_REQUEST"
