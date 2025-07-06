7de961b81cf50b0547f9c9761a79aa3aec28f303
Possanii <gpossanii@outlook.com>
2025-07-06 12:39:46 -0300

    fix(purchase): check if purchase exists before to update

--- apps/purchase/src/usecase/update-purchase.usecase.ts
+++ apps/purchase/src/usecase/update-purchase.usecase.ts
@@ -9,6 +9,10 @@ import {
   IPurchaseRepository,
   PURCHASE_REPOSITORY_TOKEN,
 } from '@app/purchase/src/repository/purchase.repo';
+import {
+  GET_PURCHASE_BY_ID_USE_CASE_TOKEN,
+  IGetPurchaseByIdUseCase,
+} from '@app/purchase/src/usecase/get-purchase-by-id.usecase';
 
 export const UPDATE_PURCHASE_USE_CASE_TOKEN = Symbol(
   'UPDATE_PURCHASE_USE_CASE',
@@ -22,12 +26,21 @@ export interface IUpdatePurchaseUseCase {
 export class UpdatePurchaseUseCase implements IUpdatePurchaseUseCase {
   constructor(
     @Inject(LOGGER_CLIENT_TOKEN) private readonly logger: ILoggerClient,
+    @Inject(GET_PURCHASE_BY_ID_USE_CASE_TOKEN)
+    private readonly getPurchaseByIdUseCase: IGetPurchaseByIdUseCase,
     @Inject(PURCHASE_REPOSITORY_TOKEN)
     private readonly purchaseRepo: IPurchaseRepository,
   ) {}
 
   async execute(id: string, purchase: UpdatePurchaseDto): Promise<void> {
     this.logger.info('Updating purchase by Id', { id });
+
+    const purchaseExists = await this.getPurchaseByIdUseCase.execute(id);
+
+    if (!purchaseExists) {
+      this.logger.warn('Purchase not found in the database', { id });
+      return;
+    }
 
     await this.purchaseRepo.update(id, purchase);
 
