444b07f3a12ba733a7baec300754f60161a833db
Possanii <gpossanii@outlook.com>
2025-08-02 10:54:17 -0300

    feat: add dynamo permissions to ecs

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -2,16 +2,15 @@ resource "aws_iam_role" "ecs_task_execution_role" {
   name = "ecs_task_execution_role"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
+    Version   = "2012-10-17",
     Statement = [
       {
-        Action = "sts:AssumeRole"
+        Action    = "sts:AssumeRole",
+        Effect    = "Allow",
         Principal = {
           Service = "ecs-tasks.amazonaws.com"
         }
-        Effect = "Allow"
-        Sid    = ""
-      },
+      }
     ]
   })
 }
@@ -19,4 +18,50 @@ resource "aws_iam_role" "ecs_task_execution_role" {
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role" "ecs_task_role" {
+  name = "ecs_task_role"
+
+  assume_role_policy = jsonencode({
+    Version   = "2012-10-17",
+    Statement = [
+      {
+        Action    = "sts:AssumeRole",
+        Effect    = "Allow",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# 2. The Policy: Defines WHAT the role can do (access DynamoDB).
+resource "aws_iam_policy" "ecs_task_dynamodb_policy" {
+  name        = "ecs-task-dynamodb-policy"
+  description = "Allows ECS tasks to access DynamoDB."
+
+  policy = jsonencode({
+    Version   = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "dynamodb:DeleteItem",
+          "dynamodb:GetItem",
+          "dynamodb:PutItem",
+          "dynamodb:Scan",
+          "dynamodb:UpdateItem"
+        ],
+        Resource = "arn:aws:dynamodb:us-east-1:${data.aws_caller_identity.current.account_id}:table/*"
+      }
+    ]
+  })
+}
+
+# 3. The Attachment: Connects the role and the policy.
+resource "aws_iam_role_policy_attachment" "ecs_task_dynamodb_attachment" {
+  role       = aws_iam_role.ecs_task_role.name
+  policy_arn = aws_iam_policy.ecs_task_dynamodb_policy.arn
 }
